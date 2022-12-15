75492ef249bc8ef73f1c91be6827292a5c936eda
sumutoni <steumutoni@gmail.com>
2022-12-15 12:49:36 -0800

    function to find uncle of node

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -23,7 +23,7 @@ binary_tree_t *sibling(binary_tree_t *node)
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (!node || ! node->parent)
+	if (!node || !node->parent)
 		return (NULL);
 	return (sibling(node->parent));
 }
