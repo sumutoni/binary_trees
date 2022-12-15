d7a44cabe1ab6235fdfbf130b12c91cd7256753a
sumutoni <54947406+sumutoni@users.noreply.github.com>
2022-12-15 21:59:42 +0200

    Update 15-binary_tree_is_full.c

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -8,14 +8,19 @@
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
+	int full = 0;
+
 	if (!tree)
 		return (0);
-	if (!tree->right && !tree->left)
-		return (1);
-
-	if (tree->right && tree->left)
-		return (binary_tree_is_full(tree->left) &&
-			binary_tree_is_full(tree->right));
-
-	return (0);
+	if ((!tree->right && !tree->left) || (tree->left && tree->right))
+	{
+		full = 1;
+		if (!tree->right && !tree->left)
+			return (full);
+		full = full && binary_tree_is_full(tree->left);
+		if (!full)
+			return (0);
+		full = full && binary_tree_is_full(tree->right);
+	}
+	return (full);
 }
