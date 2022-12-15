7a1d6557cece635255ae00037883a56004675f7a
sumutoni <steumutoni@gmail.com>
2022-12-15 12:37:42 -0800

    function to find sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -10,12 +10,11 @@
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	if (!node  || !node->parent || !node->parent->right
-		|| !node->parent->left)
+	if (!node  || !node->parent)
 		return (NULL);
 
-	if (node->parent->left->n != node->n)
+	if (node->parent->left != node)
 		return (node->parent->left);
-	if (node->parent->right->n != node->n)
+	else
 		return (node->parent->right);
 }
